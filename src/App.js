3d6f59a0eb3b40d29bbfd3d39f0ae46253b8d214
Dominik Blak <bldominikak@gmial.com>
2021-06-11 05:56:14 +0200

    header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Sections from "./Sections";
+import Header from "./Header";
 
 const tasks = [
   { id: 1, content: "przejść na React", done: false },
@@ -11,17 +12,13 @@ const hideDoneTasks = false;
 function App() {
   return (
     <main className="container">
-      <h1 className="container__header">Lista zadań</h1>
+      <Header title="Lista zadań" />
       <Sections title="Dodaj nowe zadanie" sectionContent={<Form />} />
-      <section className="section">
-        <h2 className="section__header">
-          Lista zadań
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        </h2>
-        <div className="section__content">
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        </div>
-      </section>
+      <Sections
+        title="Lista zadań"
+        sectionContent={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+      />
     </main>
   );
 }
---
+++ src/Header/index.js
@@ -0,0 +1,2 @@
+const Header = ({ title }) => <h1 className="container__header">{title}</h1>;
+export default Header;
