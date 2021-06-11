425f07f039628577b40311bb6e04d989eeead3a6
Dominik Blak <bldominikak@gmial.com>
2021-06-11 06:30:39 +0200

    props

--- src/form/index.js
+++ src/form/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-const Form = (props) => (
+const Form = () => (
   <form className="form">
     <input className="form__input " autoFocus required placeholder="Co jest do zrobienia ?" />
     <button className="form__button">Dodaj zadanie</button>
