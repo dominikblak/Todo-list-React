3e1617e2f5a382b8daeb35d24bdc69cd9279b6fe
Dominik Blak <bldominikak@gmial.com>
2021-06-15 07:23:41 +0200

    template string

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 const Tasks = (props) => (
   <ul className="tasks">
     {props.tasks.map((task) => (
-      <li key={task.id} className={`tasks__list {task.done && props.hideDone ? "tasks__list--hiden" : ""}`}>
+      <li key={task.id} className={`tasks__list ${task.done && props.hideDone ? "tasks__list--hiden" : ""}`}>
         <button className="task__button">{task.done ? "✔" : ""}</button>
         <span className={`task__content ${task.done ? "task__content--done" : ""}`}>{task.content}</span>
         <button className=" task__button  task__button--remove">🗑 </button>
