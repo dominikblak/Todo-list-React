db32ce0b583812c79a313871daa72d1c7d07ce68
Dominik Blak <bldominikak@gmial.com>
2021-10-07 07:02:39 +0200

    added new selector

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,16 +1,19 @@
 import { Wrapper, Toggle } from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
+import { toggleHideDone, setAllDone, selectHideDone, selectAreTaskEmpty, selectIsEveryTaskDone } from "../tasksSlice";
 
 const Buttons = () => {
-  const { tasks, hideDone } = useSelector(selectTasks);
+  const areTaskEmpty = useSelector(selectAreTaskEmpty);
+  const isEveryTaskDone = useSelector(selectIsEveryTaskDone);
+  const hideDone = useSelector(selectHideDone);
+
   const disptach = useDispatch();
   return (
     <Wrapper>
-      {tasks.length > 0 && (
+      {!areTaskEmpty && (
         <>
           <Toggle onClick={() => disptach(toggleHideDone())}>{hideDone ? "Pokaż" : "Ukryj"} ukończone</Toggle>
-          <Toggle onClick={() => disptach(setAllDone())} disabled={tasks.every(({ done }) => done)}>
+          <Toggle onClick={() => disptach(setAllDone())} disabled={isEveryTaskDone}>
             Ukończ wszytskie
           </Toggle>
         </>
--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { List, Item, Content, Button } from "./styled.js";
-import { selectTasks, toogleTaskDone, removeTask } from "../tasksSlice.js";
+import { selectTasks, toogleTaskDone, removeTask, selectHideDone } from "../tasksSlice.js";
 
 const TaskList = () => {
-  const { tasks, hideDone } = useSelector(selectTasks);
+  const tasks = useSelector(selectTasks);
+  const hideDone = useSelector(selectHideDone);
   const dispatch = useDispatch();
   return (
     <List>
