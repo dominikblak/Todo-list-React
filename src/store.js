8accca0dc3ad1abb8cae59560b0a073a1a85e456
Dominik Blak <bldominikak@gmial.com>
2021-10-12 06:51:20 +0200

    added getExampleTasks

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import { watchFetchExampleTasks } from "./features/tasks/tasksSaga";
 import tasksReducer from "./features/tasks/tasksSlice";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+const store = configureStore({
   reducer: {
     tasks: tasksReducer,
   },
+  middleware: [sagaMiddleware],
 });
+sagaMiddleware.run(watchFetchExampleTasks);
+export default store;
