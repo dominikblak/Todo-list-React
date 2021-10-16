d9b207373924b98fd1759f7d017dcba42dee8f01
Dominik Blak <bldominikak@gmial.com>
2021-10-16 13:03:25 +0200

    router tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
 
-export const App =()=> <Tasks/>
+export const App = () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+      </Switch>
+    </nav>
+  </BrowserRouter>
+);
