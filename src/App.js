9f0e636c11ad8de4a598a63a497c2a73fb304e79
Dominik Blak <bldominikak@gmial.com>
2021-10-16 14:47:36 +0200

    added Author

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author/Author";
 
 export const App = () => (
   <BrowserRouter>
@@ -9,10 +10,16 @@ export const App = () => (
         <li>
           <Link to="/zadania">Zadania</Link>
         </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
       </ul>
       <Switch>
         <Route path="/zadania">
           <Tasks />
+        </Route>
+        <Route path="/autor">
+          <Author />
         </Route>
       </Switch>
     </nav>
---
+++ src/features/author/Author.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+const Author = () => (
+  <Container>
+    <Header title="O autorze" />
+    <Section
+      title="Dominik Blak"
+      sectionContent={
+        <p>
+          <b>Mam na imię Dominik.</b>
+          <br />
+          Obecnie mieszkam oraz pracuję w Krakowie. Informatyka to także moje hobby, dlatego postanowiłem połączyć
+          przyjemne z pożytecznym i się przebranżowić. Miałem już styczność z<i>Javą</i>
+          oraz
+          <i>Angularem,</i>
+          <strong>
+            jednak najbardziej przypdał mi do gustu
+            <i>JavaScript</i>i<i>React.</i>
+          </strong>
+        </p>
+      }
+    />
+  </Container>
+);
+export default Author;
