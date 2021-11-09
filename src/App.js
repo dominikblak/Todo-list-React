e960e15569e7e6883cc31a25521bdaf2bf7873fd
Dominik Blak <bldominikak@gmial.com>
2021-11-09 18:17:33 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 import AuthorPage from "./features/author/AuthorPage";
-import { Navigation } from "./features/Navigation";
+import { Navigation } from "./features/navigation";
 
 export const App = () => (
   <HashRouter>
